c3056ab221534df47b361d23b0419a08d6f4941e
matts112 <mttswinford@yahoo.com>
2019-01-09 12:40:57 -0700

    second commit

---
+++ build/precache-manifest.070855c55ee99e7cb8d3cf39e6ea8a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42cd347bd6a3ffe95e4f",
+    "url": "/static/css/main.e4241742.chunk.css"
+  },
+  {
+    "revision": "42cd347bd6a3ffe95e4f",
+    "url": "/static/js/main.42cd347b.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62a2721968ae47bbe9633f6834ce2d29",
+    "url": "/index.html"
+  }
+];
